c88c2a70e33423f059d2f0a400d9f30ed201708f
Carlos A. Gómez Urda <carlosurda@tutamail.com>
2025-12-23 13:34:22 +0100

    Refactorizando config.init

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -7,37 +7,53 @@ local M = {}
 
 
 -- Crear una nueva instancia de configuración para un plugin específico
--- @param plugin_mod string Nombre del módulo del plugin dentro de su repositorio: lua/<plugin>.lua o lua/<plugin>/init.lua
+-- @param plugin_mod string Nombre del módulo del plugin dentro de su repositorio: lua/<plugin>.lua
+-- o lua/<plugin>/init.lua
 -- @param clave_ruta_config string Ruta en dot notation (formato directorios separados por '.') dentro
 -- del packagepath donde se encuentran los archivos de configuración de usuario del plugin.
--- Si nil no se podrá acceder a archivos de configuración (para cuando se vayan a usar los valores
--- de configuracion por defecto)
+-- nil si no se van a acceder a archivos de configuración del usuario (para cuando se vayan a usar
+-- los valores de configuracion por defecto del plugin)
 -- @return table Instancia de configuración con métodos para cargar opciones y plugins.
 --  -- Nil en caso de error
 function M.new(plugin_mod, clave_ruta_config)
     -- La comprobación de la existencia del módulo plugin.lua o plugin/init.lua se hace en el setup justo antes de intentar
     -- cargar el módulo, y no ahora que puede todavía no existir.
-    if type(plugin_mod) ~= "string" or plugin_mod == "" then
-        return nil, "El nombre del plugin no es una cadena no vacía"
+    if type(plugin_mod) ~= "string" then 
+        return nil, "El nombre del plugin no es una cadena"
+    end
+    plugin_mod = vim.trim(plugin_mod)
+    if plugin_mod == "" then
+        return nil, "El nombre del plugin es una cadena vacía"
     end
 
-    if clave_ruta_config ~= nil and type(clave_ruta_config) ~= "string" then
-        return nil, "La clave de la ruta de los archivos de configuración no es una cadena"
+    if type(clave_ruta_config) ~= "string" then
+        if clave_ruta_config ~= nil then
+            return nil, "La clave de la ruta de los archivos de configuración no es una cadena"
+        end
+    else
+        clave_ruta_config = vim.trim(clave_ruta_config)
     end
+
 
     -- Función para comprobar y cargar un módulo de configuración específico del plugin
-    -- @param modulo string Nombre del módulo a cargar (sin extensión .lua)
+    -- @param modulo string Nombre del módulo a cargar (sin extensión .lua).
+    -- nil hace que la función devuelva nil, usado fuera para tomar valores por defecto.
+    -- "" se considera error.
     -- @return boolean ok Indica si la carga fue exitosa
     -- @return table|nil res Tabla con el contenido del módulo cargado, o nil si no se encontró
     -- @return number|nil log_level Nivel de log (vim.log.levels) en caso de error
     -- @return string|nil msg Mensaje de error en caso de error
     local function _comprobar_modulo(modulo)
-        if type(modulo) ~= "string" or modulo == "" then
-            return false, nil, vim.log.levels.ERROR, "Nombre de módulo de configuración debe ser una cadena no vacía"
+        if clave_ruta_config == nil or modulo == nil then
+            return true, nil
         end
 
-        if clave_ruta_config == nil then
-            return true, nil
+        if type(modulo) ~= "string" then
+            return false, nil, vim.log.levels.ERROR, "Nombre de módulo de configuración debe ser una cadena"
+        end
+        modulo = vim.trim(modulo)
+        if modulo == "" then
+            return false, nil, vim.log.levels.ERROR, "Nombre de módulo de configuración no puede ser cadena vacía"
         end
 
         local clave_modulo = clave_ruta_config .. "." .. modulo
@@ -65,7 +81,7 @@ function M.new(plugin_mod, clave_ruta_config)
     -- @return any res Opciones cargadas, o nil si no se encontró
     -- @return number|nil log_level Nivel de log (vim.log.levels) en caso de error
     -- @return string|nil msg Mensaje de error en caso de error
-    local function get_config(modulo, pre_opts)
+    local function exec_modulo(modulo, pre_opts)
         local ok, res, log_level, msg = _comprobar_modulo(modulo)
 
         if res == nil then
@@ -85,7 +101,7 @@ function M.new(plugin_mod, clave_ruta_config)
     -- @param plugin_spec table Tabla con la especificación del plugin (usado en Lazy)
     -- @param opts table Tabla con las opciones de configuración del plugin
     -- @return nil
-    local function callback_setup(plugin_spec, opts)
+    local function setup(plugin_spec, opts)
         local ok, res = pcall(require, plugin_mod)
         if not ok then
             vim.notify("Error al cargar el módulo de setup " .. plugin_mod .. ": " .. tostring(res), vim.log.levels.ERROR)
@@ -103,13 +119,19 @@ function M.new(plugin_mod, clave_ruta_config)
     end
 
 
-    -- Obtener una función de callback para cargar las opciones de un  módulo de configuración del plugin
-    -- @param modulo string Nombre del módulo a cargar (sin extensión .lua)
-    -- @param pre_datos table Tabla con datos previos para combinar con las opciones cargadas
+    -- Obtener una función de callback genérica para asignar a los campos de la especificación de
+    -- un plugin o del módulo bootstrao, y que al ejecutar se obtengan datos o ejecuten acciones
+    -- asociadas a ese campo
+    -- @param modulo_pre string Nombre del módulo a ejecutar (sin extensión .lua) antes de realizar
+    -- el setup. Los valores que devuelva este módulo serán ignorados. nil no ejecuta nada.
+    -- @param setup string Nombre de la función setup del módulo del plugin
+    -- @param modulo_post string Nombre del módulo a ejecutar (sin extensión .lua) después de
+    -- realizar el setup. Los valores que devuelva este módulo serán retornados por el callback. 
+    -- nil no ejecuta nada y hace que el callback devuelva nil.
     -- @param min_name_log_level string Nivel mínimo de log para notificaciones, en mayúsculas o minúsculas:
     -- ("TRACE", "DEBUG", "INFO", "WARN", "ERROR", "OFF")
     -- @return function Función de callback que carga las opciones cuando se llama
-    local function get_callback_get_config(modulo, min_name_log_level)
+    local function get_callback(modulo_pre, setup, modulo_post, min_name_log_level)
         local min_log_level = vim.log.levels[tostring(min_name_log_level):upper()] or vim.log.levels.WARN
 
         local function callback(plugin_spec, pre_opts)
