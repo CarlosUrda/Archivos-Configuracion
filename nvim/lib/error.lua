541bb92da708371f438d0063ed2d1c3a2cfd6283
Carlos A. Gómez Urda <ged@keemail.me>
2026-01-25 20:25:46 +0100

    Seguimos con el módulo error y los assert

--- nvim/lib/error.lua
+++ nvim/lib/error.lua
@@ -10,38 +10,50 @@ function E.noop() end
 
 E.NIL = {}
 
-E.debug = false
+_prv[E].debug = false
 E.assert_dbg = E.debug and assert or E.noop
 E.assertf_dbg = E.debug and E.assertf or E.noop
 
-_prv[E]_TIPO = {
+_prv[E].INFO_TIPO_ERR = {
     ERR = {
-        msg  = "Error",
-        TIPO = {
-            msg  = "Tipo de dato inválido",
-        },
-        SPEC = {
-            msg = "Inconsistencia en la especificación",
-        }
-        VALOR = {
-            msg = "Valor inválido",
-        }
-        META = {
-            msg = "Error al preparar el lanzamiento de un error",
-            ARG = _prv[E].TIPO.ERR.ARG,
-            TIPO = _prv[E].TIPO.ERR.TIPO,
-            VALOR = _prv[E].TIPO.ERR.VALOR,
-        },
-        ARG = {
-            msg  = "Inconsistencia en argumento",
-            TIPO = _prv[E].TIPO.ERR.TIPO,
-            VALOR = _prv[E].TIPO.ERR.VALOR,
-        }
+        msg = "Error",
+    },
+    TIPO = {
+        msg = "Tipo de dato inválido",
+    },
+    SPEC = {
+        msg = "Inconsistencia en la especificación",
+    }
+    VALOR = {
+        msg = "Valor inválido",
+    }
+    META = {
+        msg = "Error al preparar el lanzamiento de un error",
+    },
+    ARG = {
+        msg = "Inconsistencia en argumento",
     }
 }
 
+
+-- Comprueba la veracidad de un valor.
+-- @param valor any Promociona a boolean. Comprueba si el valor se considera como true
+-- @return boolean true si se considera como verdadero y false si se considera como falso.
+function E.es_true(valor)
+    return valor ~= false and valor ~= nil
+end
+
+-- Comprueba la falsedad de un valor.
+-- @param valor any Promociona a boolean. Comprueba si el valor se considera como false
+-- @return boolean true si se considera como falso y false si se considera como verdadero.
+function E.es_false(valor)
+    return not E.es_true(valor)
+end
+
+
 -- Comprueba si una función es un número entero.
--- @param 
+-- @param numero any Valor a comprobar si es un número entero.
+-- @return boolean Si es entero devuelve true, si no lo es false.
 function E.es_entero(numero)
     return type(numero) == "number" and math.floor(numero) ~= numero
 end
@@ -50,11 +62,12 @@ end
 -- Comprobar si un valor es igual a otro valor, dando la posibilidad de aceptar nil. 
 -- @param valor any Valor a comprobar
 -- @param valor_cmp any Valor con el cual comparar.
--- @param nil_valido boolean|nil Indica si se admite el valor nil como válido. Por defecto inválido.
--- @return boolean true si los valóres son iguales, o es nil y nil_valido es true.
+-- @param nil_valido any Promociona a boolean. Si es verdadero, admite el valor nil como válido.
+-- -- Si es falso el valor nil no se considera válido. Por defecto inválido.
+-- @return boolean true si los valóres son iguales, o es nil y nil_valido es verdadero.
 -- @throws Error si los argumentos no son del tipo adecuado.
-local function es_valor(valor, valor_cmp, nil_valido)
-    return valor == valor_cmp or (nil_valido ~= false and nil_valido ~= nil)
+function E.es_valor(valor, valor_cmp, nil_valido)
+    return valor == valor_cmp or (es_true(nil_valido) and valor == nil)
 end
 
 
@@ -64,24 +77,48 @@ end
 -- @param nil_valido boolean|nil Indica si se admite el valor nil como válido. Por defecto inválido.
 -- @return boolean true si el valor es del tipo esperado o es nil y nil_valido es true.
 -- @throws Error si los argumentos no son del tipo adecuado.
-local function es_tipo(valor, tipo, nil_valido)
-    assertf_dbg(type(tipo) ~= "string", "ERR.META.ARG.TIPO",
+function E.es_tipo(valor, tipo, nil_valido)
+    assertf_dbg(type(tipo) == "string", "ERR.META.ARG.TIPO",
         "El segundo argumento (tipo a comprobar) debe ser string", "STRING_ESPERADO", 
-        {var = "tipo", val = tipo}, 3)
+        {var = "tipo", val = tipo, fun = "E.es_tipo"}, 2)
     
-    return type(valor) == tipo or (nil_valido and valor == nil)
+    return E.es_valor(type(valor), tipo, nil_valido)
 end
 
 
--- Obtener la información relacionada a los tipos de errores predefinidos: el valor del tipo
--- de error que va dentro del objeto error lanzado y el mensaje por defecto.
-function E.obtener_msg(tipo)
-    assertf_dbg(type(tipo) ~= "string", "ERR.ARG.TIPO", "El primer argumento (tipo) debe ser string",
-        "STRING_ESPERADO", {var = "tipo", val = tipo}, 3)
+-- Cambiar el flag debug para ejecutar el módulo en modo debug.
+-- @param estado any Valor del estado considerado como boolean.
+-- @return boolean el estado promocionado a boolean.
+function E.cambiar_debug(estado)
+    _prv[E].debug = E.es_true(estado)
 
-    msg = ""
-    for subtipo in string.gmatch(tipo, "^[_%a]+"
+    return _prv[E].debug
+end
 
+-- Obtener el mensaje por defecto asociado a cada tipo de error predefinido
+-- @param string Tipo de error (subtipos de error separados por punto)
+-- @return string Mensaje por defecto asociado a ese tipo de error.
+-- @throws {tipo, msg, codigo, {var, val, , subval, fun}}
+function _prv[E].obtener_msg(tipo_err)
+    local extra = {var = "tipo_err", val = tipo_err, fun = "E.obtener_msg"}
+    assertf_dbg(type(tipo_err) == "string", "ERR.META.ARG.TIPO", "El primer argumento (tipo de error) debe ser string", "STRING_ESPERADO", extra, 2)
+
+    local tipo_err_norm = tipo_err:match("^%s*([%w_]+(%.[%w_]+)*)%s*$") 
+    assertf_dbg(tipo_err_norm ~= nil, "ERR.META.ARG.VALOR", "El primer argumento (tipo de error) debe estar en formato correcto", "FORMATO_INVALIDO", 2)
+    tipo_err_norm = tipo_err_norm:upper()
+
+    local msg = ""
+    local info_tipo_err = _prv[E].INFO_TIPO_ERR
+    for subtipo_err in string.gmatch(tipo_err_norm, "[_%w]+") do
+        extra.subval = subtipo_err
+        local info_subtipo_err = info_tipo_err[subtipo_err]
+        assertf_dbg(type(info_subtipo_err) == "table", "ERR.META.ARG.VALOR", "El subtipo de error %s del primer argumento (tipo de error) no es un tipo predefinido":format(subtipo_err), "SUBVALOR_INVALIDO", extra, 2) 
+        msg_subtipo = info_subtipo_err.msg
+        assertf_dbg(type(msg_subtipo) == "string", "ERR.META.SPEC", "El campo msg por defecto del subtipo de error %s no es una cadena":format(subtipo_err), "STING_ESPERADO", extra, 2) 
+        msg = msg .. msg_subtipo .. ":"
+    end
+    
+    return msg:match("(.*):")
 end
 
 function E.crear_err(tipo, codigo, msg, extra)
@@ -124,18 +161,20 @@ end
 
 
 function E.assertf(cond, info, nivel)
+    assertf_dbg(E.es_entero(nivel), 
+        function() return {"ERR.META.ARG.TIPO", "El tercer parametro (nivel) debe ser un entero", "ENTERO_ESPERADO",
+            {var = "nivel", val = nivel, fun = "E.assertf"} end, 2)
+
     if cond then return end
 
-    if type(info) == "string" then
-        tipo, msg_defecto = E.obtener_tipo(tipo)
-    assertf_dbg(type(funcion) == "function", "ERR.ARG.TIPO", "El segundo parametro debe ser una función",
-        "FUNCION_ESPERADA", {var = "funcion", val = funcion}, 3)
+    if type(info) == "function" then
+        err = info()
+    else
+        err = info
 
-
-    if not cond then funcion()
-        error( , nivel)
-    end
+    error(err, nivel)
 end
+
 
 function E.assertf(cond, tipo, msg, codigo, extra, nivel)
 
